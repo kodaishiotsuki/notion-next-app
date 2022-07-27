fbe01db9b6490730a1ea16eea18116bc9e2f4db2
kodaishiotsuki <0711dr5@gmail.com>
2022-07-27 22:26:24 +0900

    getServerSideProps

--- types/types.ts
+++ types/types.ts
@@ -1,17 +1,27 @@
 import { ReactNode } from "react";
+import { ParsedUrlQuery } from "querystring";
 
 export type LayoutProps = {
   children: ReactNode;
 };
 
+export type PageProps = {
+  slug: string;
+  name: string;
+  author: string;
+  cover: string;
+  published: string;
+  tags: string[];
+  content: string;
+};
+
 export type CardProps = {
-  page: {
-    slug: string;
-    name: string;
-    author: string;
-    cover: string;
-    published: string;
-    tags: string[];
-    content: string;
-  };
+  page: PageProps;
+};
+
+export type ArticleProps = CardProps;
+export type ArticleMetaProps = CardProps;
+
+export type Params = ParsedUrlQuery & {
+  slug: string;
 };
