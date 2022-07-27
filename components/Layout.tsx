e8f5f76992cfd4484c3f140b589ccce393079985
kodaishiotsuki <0711dr5@gmail.com>
2022-07-27 21:24:19 +0900

    children

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
-import React from "react";
+import React, { FC } from "react";
+import { LayoutProps } from "../types/types";
 import { Footer } from "./Footer";
 import NavBar from "./NavBar";
 
-const Layout = () => {
+
+
+const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="relative overflow-hidden">
       <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
         {/*  */}
         <NavBar />
         {/*  */}
-        <main></main>
+        <main className="w-full pb-12 px-4">{children}</main>
         {/*  */}
         <Footer />
       </div>
